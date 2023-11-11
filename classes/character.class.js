3fcfab03f9da910abbadb5e1acee5e37ce599445
Chrizly311 <117831221+Chrizly311@users.noreply.github.com>
2023-11-11 12:48:24 +0100

    animated character

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,13 +1,36 @@
 class Character extends MovableObject{
     width = 150;
     height = 200;
+    IMAGES_WALKING = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'
+    ];
+    currentImage = 0;
 
     constructor(x, y) {
-        super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
+        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.animate();
+        
         this.x = x;
         this.y = y;
     }
-    
+
+    animate() {
+        setInterval(() => {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+        }, 100)
+    }
+
     jump() {
 
     }
--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -4,11 +4,25 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
 
     //load image
     loadImage(path) {
         this.img = new Image(); //ist das gleiche wie document.getElementByID('image')...
         this.img.src = path;
+    }
+
+    /**
+    * 
+    * @param {Array} arr - ['img/image1.png', 'img/image2.png']
+    */
+    
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path; 
+            this.imageCache[path] = img;
+        });
     }
 
     moveRight() {
