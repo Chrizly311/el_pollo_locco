3d8ec3bb190238654604103fe2837e28e6054e84
Chrizly311 <117831221+Chrizly311@users.noreply.github.com>
2023-11-14 23:23:33 +0100

    implemented eventlistener

--- classes/world.class.js
+++ classes/world.class.js
@@ -18,11 +18,18 @@ skies = [
 ];
 canvas;
 ctx;
+keyboard;
 
-    constructor(canvas) {
+    constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
+        this.keyboard = keyboard;
         this.draw();
+        this.setWorld();
+    }
+
+    setWorld() {
+        this.character.world = this;
     }
 
     draw() {
